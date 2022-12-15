8b8659db09283a2f4fdb690ffd8616bbed51d634
ACMCMC <20495460+ACMCMC@users.noreply.github.com>
2022-12-15 17:34:25 +0100

    notes list

--- src/NotesList.tsx
+++ src/NotesList.tsx
@@ -22,14 +22,23 @@ export class NotesList extends Component<NotesListProps> {
     }
 
     return (this.props.notes.map((note, key) =>
-      <div className="row g-0 border rounded m-5 p-5" key={key}>
-        <div className="d-flex col-lg-4 order-lg-1">
-          <img src={noteIcon} className="mx-auto my-auto" alt="Icon of a note" />
+      <div className="card m-5" key={key}>
+        <div className="card-header">
+          {note.uuid}
         </div>
-        <div className="col-lg-8 order-lg-2 my-auto showcase-text">
-          <h2>{note.title}</h2>
-          <p className="small my-0">Owner: {note.owner}</p>
-          <p className="lead mb-0">{note.description}</p>
+        <div className="card-body">
+          <div className="d-flex p-0">
+              <div className="col flex-grow-0 ps-3 pe-4">
+                <img src={noteIcon} className="mx-auto" alt="Notes icon" style={{ height: "auto", width: "auto" }}></img>
+              </div>
+              <div className="col flex-fill">
+                <h5 className="card-title">{note.title}</h5>
+                <p className="card-text">{note.description}</p>
+              </div>
+            </div>
+        </div>
+        <div className="card-footer text-muted">
+          Owner: {note.owner}
         </div>
       </div>
     ));
